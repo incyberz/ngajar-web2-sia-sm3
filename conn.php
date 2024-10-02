a94c2c2fdbd1355e0571bb0ba81d3a7bf3ed52a6
incyberz <incyberz@yahoo.com>
2024-10-02 13:57:44 +0700

    pweb2-create-read-delete

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+$cn = mysqli_connect(
+  'localhost',
+  'root',
+  '',
+  'db_web2_sia_p2'
+);
---
+++ index.php
@@ -0,0 +1,121 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<h1 class="text-center">Tampil Data Peserta</h1>
+<?php
+
+include 'conn.php';
+
+# ============================================================
+# PROCESSORS
+# ============================================================
+if (isset($_POST['btn_delete'])) {
+  $s = "DELETE FROM tb_peserta WHERE id=$_POST[btn_delete]";
+  $q = mysqli_query($cn, $s) or die(mysqli_error($cn));
+
+  echo "<div class='alert alert-success'>Delete berhasil</div>";
+
+  // redirect ke index.php
+  echo "
+    <script>
+      location.replace('index.php')
+    </script>
+  ";
+}
+
+if (isset($_POST['btn_add'])) {
+
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
+
+  $s = "INSERT INTO tb_peserta (
+    nama,
+    ip_address,
+    tipe_gadget
+  ) VALUES (
+    '$_POST[nama]',
+    '$_POST[ip_address]',
+    $_POST[tipe_gadget]
+
+  )";
+
+  $q = mysqli_query($cn, $s) or die(mysqli_error($cn));
+
+  echo "<div class='alert alert-success'>Add berhasil</div>";
+
+  // redirect ke index.php
+  echo "
+    <script>
+      location.replace('index.php')
+    </script>
+  ";
+}
+
+
+
+
+# ============================================================
+# MAIN SELECT | TAMPIL DATA
+# ============================================================
+$s = "SELECT * FROM tb_peserta ";
+$q = mysqli_query($cn, $s) or die(mysqli_error($cn));
+
+// looping
+$tr = '';
+$i = 0;
+while ($d = mysqli_fetch_assoc($q)) {
+  $i++;
+  $tr .= "
+    <tr>
+      <td>$i</td>
+      <td>$d[nama]</td>
+      <td>$d[ip_address]</td>
+      <td>$d[tipe_gadget]</td>
+      <td>$d[date_created]</td>
+      <td>
+        <button class='btn btn-sm btn-info'>Edit</button>
+
+        <form method=post style='display:inline'>
+          <button name=btn_delete value=$d[id] class='btn btn-sm btn-danger'  onclick='return confirm(`Yakin mau hapus peserta ini?`)' >Delete</button>
+        </form>
+      </td>
+    </tr>
+  ";
+}
+
+
+# ============================================================
+# FORM TAMBAH DATA
+# ============================================================
+$tr .= "
+  <tr>
+    <td colspan=100%>
+      <form method=post>
+        <input name=nama placeholder='Nama'>
+        <input name=ip_address placeholder='IP Address'>
+        <input name=tipe_gadget placeholder='Tipe Gadget'>
+        <button name=btn_add>Add</button>
+      </form>
+    </td>
+  </tr>
+";
+
+
+# ============================================================
+# FINAL UI
+# ============================================================
+echo "
+  <div class=container>
+    <table class='table table-striped table-hover'>
+      <thead>
+        <th>No</th>
+        <th>Nama</th>
+        <th>IP</th>
+        <th>Gadget</th>
+        <th>Date Created</th>
+        <th>Aksi</th>
+      </thead>
+      $tr
+    </table>
+  </div>
+";
